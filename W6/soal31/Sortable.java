bbed945a90e5e8601b83f69f262b72e0f6a8e497
Rifqi Irfansyah <rifqiirfansyah518@gmail.com>
2024-09-26 17:52:01 +0700

    add short able code

--- W6/soal31/Sortable.java
+++ W6/soal31/Sortable.java
@@ -2,8 +2,20 @@ package W6.soal31;
 
 abstract class Sortable{
     public abstract int compare(Sortable b);
-    public static void shell_sort(Sortable[] a){
-    //Shell sort body
+
+    public static void shell_sort(Sortable[] a) {
+        int n = a.length;
+        // Implementasi algoritma shell sort
+        for (int gap = n / 2; gap > 0; gap /= 2) {
+            for (int i = gap; i < n; i++) {
+                Sortable temp = a[i];
+                int j;
+                for (j = i; j >= gap && temp.compare(a[j - gap]) < 0; j -= gap) {
+                    a[j] = a[j - gap];
+                }
+                a[j] = temp;
+            }
+        }
     }
 }
    
